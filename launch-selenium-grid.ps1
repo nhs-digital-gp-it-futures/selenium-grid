d5ce309649d7ae4a94e74cb5e7b50de77ea07617
Adrian Soomro <adrian.soomro@gmail.com>
2020-07-22 15:29:09 +0100

    Move process to a release pipeline instead
    AB#7961
    AB#8703

--- launch-selenium-grid.ps1
+++ launch-selenium-grid.ps1
@@ -6,4 +6,4 @@ if ($response -ne "")
     kubectl apply -f grid-namespace.yml
 }
 
-helm upgrade sel-grid stable/selenium -i -f values.yaml -n selenium-grid
+helm upgrade sel-grid stable/selenium -i -f values.yaml -n $namespace
